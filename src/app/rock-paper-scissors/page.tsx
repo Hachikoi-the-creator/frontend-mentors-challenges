4b489c6468fce5009a8bfa1403070dd768f14213
Adan Moreno <adan.moreno@heyhom.mx>
2025-08-22 10:35:33 -0600

    Feat: removed logs

--- src/app/rock-paper-scissors/page.tsx
+++ src/app/rock-paper-scissors/page.tsx
@@ -1,6 +1,24 @@
 import ScoreTab from "./components/ScoreTab";
 import "./rps-style.scss";
 
+type Props = {
+  catalogSchema: Record<string, any>;
+};
+
+export type FixedCatalogData = {
+  id: string;
+  name?: string;
+  images?: string[];
+  brand?: string;
+  title?: string;
+  currency?: string;
+  price?: number;
+  availability?: {
+    available_tag?: string;
+    available_color?: string;
+  };
+} & Record<string, any>;
+
 export default function page() {
   return (
     <main className="h-screen bg-gradient pt-8">
